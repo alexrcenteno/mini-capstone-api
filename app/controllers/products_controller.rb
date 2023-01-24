a5f8ae8b6a579b84fd01c57d03c6a9cd3377f0c4
alexrcenteno <alex.centeno1991@gmail.com>
2023-01-24 13:20:49 -0600

    adds happy and sad path for update action. adds test for sad paths in update action. adds happy sad paths for create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,23 +11,31 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(
-      name: params["name"],
-      price: params["price"],
-      image_url: params["image_url"],
-      description: params["description"],
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
     )
-    render :show
+    if @product.valid?
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
-    @product = Product.find_by(id: params["id"])
+    @product = Product.find_by(id: params[:id])
     @product.update(
-      name: params["name"] || @product.name,
-      price: params["price"] || @product.price,
-      image_url: params["image_url"] || @product.image_url,
-      description: params["description"] || @product.description,
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description,
     )
-    render :show
+    if @product.valid?
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,13 +14,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "image_url", "description", "is_discounted?", "tax", "total", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "name", "price", "image_url", "description", "is_discounted?", "tax", "total", "quantity", "created_at", "updated_at"], data.keys
   end
 
   test "create" do
     assert_difference "Product.count", 1 do
       post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
     end
+    post "/products.json", params: {}
+    assert_response 422
   end
 
   test "update" do
@@ -33,6 +35,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal product.price.to_s, data["price"]
     assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
+    assert_equal product.quantity, data["quantity"]
+
+    patch "/products/#{product.id}.json", params: { price: 0 }
+    assert_response 422
   end
 
   test "destroy" do
