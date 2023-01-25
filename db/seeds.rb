5a690c5b5e002ac7eecdadd29c2f03bb85c44979
alexrcenteno <alex.centeno1991@gmail.com>
2023-01-25 12:45:01 -0600

    adds new model called supplier. Added 3 suppliers to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,5 @@ Product.destroy_all
 Product.create!([
   { name: "pen", price: "3.0", image_url: "image.jpeg", description: "a tool to write or draw on a piece of paper" },
   { name: "Screwdriver", price: "9.0", image_url: "image.jpeg", description: "a tool used to drive screws into surfaces" },
-  { name: "Pencil", price: "1.0", image_url: "test.jpg", description: "Instrument used to write or draw." },
-  { name: "test", price: "1.0", image_url: "test", description: "test" },
-  { name: "test", price: "1.0", image_url: "test", description: "test" },
-  { name: "test", price: "1.0", image_url: "test", description: "test" },
-  { name: "test", price: "1.0", image_url: "test", description: "test" },
-  { name: "test", price: "1.0", image_url: "test", description: "test" },
+
 ])
